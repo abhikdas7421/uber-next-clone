71c71ecc54e72c4904f4e935a872dc923a551df1
Abhik Das <abhikdas7421@gmail.com>
2021-11-05 20:49:41 +0530

    Modified pages/index.js -> Create Headers section

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,49 @@ export default function Home() {
 	return (
 		<Wrapper>
 			<Map />
-			<ActionItems>Start</ActionItems>
+			<ActionItems>
+				{/* {Headers} */}
+				<Header>
+					<UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+
+					<Profile>
+						<Name>Abhik Das</Name>
+						<UserImage src="https://img.icons8.com/color/96/000000/user-male-circle--v2.png" />
+					</Profile>
+				</Header>
+
+				{/* {ActionButton} */}
+
+				{/* {InputButton} */}
+			</ActionItems>
 		</Wrapper>
 	);
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen
+  flex flex-col h-screen
 `;
 
 const ActionItems = tw.div`
-	flex-1
+	flex-1 p-4
+`;
+
+const Header = tw.div`
+	flex justify-between intems-center
+`;
+
+const UberLogo = tw.img`
+	h-28
+`;
+
+const Profile = tw.div`
+	flex items-center
+`;
+const Name = tw.div`
+	mr-4 w-20 
+`;
+
+const UserImage = tw.img`
+	h-12 w-12 border border-gray-200 p-px
+
 `;
